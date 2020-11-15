8374deef12142be9a0afe9542825e5a610001d4e
Sheb <michael@sheb.rocks>
2020-11-15 19:42:49 +0100

    added missing test for gate

--- __tests__/2d_overlap_merge.test.js
+++ __tests__/2d_overlap_merge.test.js
@@ -29,7 +29,20 @@ describe("merge_overlaps", () => {
     expect(actual).toStrictEqual(expected)
   })
 
-  it.todo("runs acceptably fast for larger inputs")
+  it("is idempotent", () => {
+    const original_input = [[3,6],[13,20],[7,10],[6,11],[1,12],[0,9],[0,1],[3,12],[0,5],[6,17],[16,22],[0,1],[4,17],[5,10],[10,20],[3,5],[7,12],[2,5],[8,11],[5,16]]
+    const expected = [[2,23], [25,30]]
+
+    const input = merge_overlaps(original_input)
+    const actual = merge_overlaps(input)
+
+    expect(actual).toStrictEqual(expected) 
+  })
+
+  it.todo("runs acceptably fast for larger inputs", () => {
+    const original_input = Array.from({length: 1000}, () => { const end = randomInt(10000); return [randomInt(end), end]})
+    
+  })
   it.todo("does not mutate input")
   it.todo("paralellizes well")
 })
