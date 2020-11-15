e1e499205fa37771738688e32699199a4ebee61d
Sheb <michael@sheb.rocks>
2020-11-15 18:58:38 +0100

    added specs

---
+++ __tests__/2d_overlap_merge.test.js
@@ -0,0 +1,23 @@
+const { describe, it } = require("@jest/globals");
+
+describe("2d_overlap_merge", () => {
+  it("defaults to an empty array", () => {
+
+  })
+
+  it("merges correctly for normalized entries", () => {
+
+  })
+
+  it("runs acceptably fast for larger inputs", () => {
+
+  })
+
+  it("does not mutate input", () => {
+
+  })
+
+  it("paralellizes well", async () => {
+
+  })
+})
